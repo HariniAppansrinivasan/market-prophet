3e83e99a83d28cc262ca4809ffe9fc58507fdd97
Ruze Richards <ruze00@gmail.com>
2021-02-21 12:09:36 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+conda create -n prophet python=3 pandas numpy scikit-learn matplotlib plotly jupyter
+conda install -c anaconda ephem
+conda install -c conda-forge pystan
+conda install -c conda-forge fbprophet
